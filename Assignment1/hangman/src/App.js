beab88d74e619cb47ab7eafb4e55593050b146fb
mariyaeggs <mariyawilson@att.net>
2018-03-04 14:30:05 -0800

    Set React state from user input.

--- Assignment1/hangman/src/App.js
+++ Assignment1/hangman/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(event) {
+    console.log('setting state')
+    console.log(event)
+    this.setState({ value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('You submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hangman</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, guess a letter.
         </p>
+        <form onSubmit={ this.handleSubmit} >
+          <label>
+            Letter:
+            <input
+              type="text"
+              value={ this.state.value }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
