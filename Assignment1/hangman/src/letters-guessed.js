b9aa2fb066aa8e63f60ed630f5874afc4950cd83
mariyaeggs <mariyawilson@att.net>
2018-03-06 18:05:37 -0800

    Create component to display letters guessed

---
+++ Assignment1/hangman/src/letters-guessed.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+
+const LettersGuessed = (props) => {
+  // Destructure answerValue from this.props
+  const { lettersGuessed } = props;
+  const displayedAnswer = lettersGuessed.join(' ');
+  return (
+    <div>
+      <h4>
+        Letters already guessed:
+        <br />
+        { displayedAnswer }
+      </h4>
+    </div>
+  );
+};
+LettersGuessed.propTypes = {
+  lettersGuessed: PropTypes.string.isRequired,
+};
+
+export default LettersGuessed;
